03d0d94ab9e0e2fff89b6f5b7ac0b71e1da74652
metalx1000 <github@filmsbykris.com>
2016-02-21 11:55:37 -0500

    added car speed and direction

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ var game = new Phaser.Game(360, 640, Phaser.AUTO, 'phaser', { preload: preload, 
 var centerx = game.width / 2;
 var centery = game.height / 2;
 var players = [];
+var cars;
+var lastLane = 0;
+var ispeed = 10;
+var lanes = [.1,.4,.65,.9];
+var tileSpeed = ispeed * .1; 
 var road;
 var sprites = [
   "eng",
@@ -25,12 +30,19 @@ function create() {
   road = game.add.tileSprite(0, 0, game.width, game.height, 'road');
   createPlayer(150);
   game.input.onDown.add(go_fullscreen, this);
+  cars = game.add.group();
+  cars.enableBody = true;
+
+  //add cars on loop
+  game.time.events.add(3000, createCar, this);
 }
 
 function update(){
   movePlayers();
 
-  road.tilePosition.y += 2;
+  tileSpeed = ispeed * .1;
+  if(tileSpeed > 3){titleSpeed = 3}
+  road.tilePosition.y += tileSpeed;
 }
 
 function movePlayers(){
@@ -62,3 +74,25 @@ function go_fullscreen(){
   game.scale.startFullScreen();
 }
 
+function createCar(){
+  //time until next car
+  var t = Math.floor(Math.random() * 4000) + 1000;
+  t = t - ispeed * 10;
+  if(t < 200){t=200}; 
+  game.time.events.add(t, createCar, this);
+  ispeed += 5;
+  var n = Math.floor(Math.random() * 7) + 1;
+  var l = Math.floor(Math.random() * 4);
+  if( lastLane != l ){
+    lastLane = l;
+    var car = cars.create(game.width * lanes[l], -100, "car" + n);
+    car.anchor.setTo(.5,.5);
+    if(l < 2){
+      car.angle = 180;
+      var speed = 150 + ispeed;
+    }else{
+      var speed = 50 + ispeed;
+    }
+    car.body.velocity.y = Math.floor(Math.random() * 30) + speed;
+  }
+}
