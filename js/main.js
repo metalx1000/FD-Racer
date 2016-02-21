11c2fa32009fd9a459efe0120c47b322ef0d0cd3
metalx1000 <github@filmsbykris.com>
2016-02-21 10:20:20 -0500

    added player

--- js/main.js
+++ js/main.js
@@ -20,9 +20,35 @@ function preload() {
 }
 
 function create() {
-  
+  createPlayer(150);
 }
 
 function update(){
+  movePlayers();
+}
 
+function movePlayers(){
+  players.forEach(function(player){
+    if(player.body.x != game.input.x){
+      game.physics.arcade.moveToPointer(player, 400);
+    }
+    player.body.y = player.posY;
+    //stop movement when player reaches cursor 
+    /*
+    if (Phaser.Rectangle.contains(player.body, game.input.x, game.input.y))
+    {   
+        player.body.velocity.setTo(0, 0);
+    }
+    */
+  })
+}
+
+function createPlayer(y){
+  var player = game.add.sprite(game.width/2, game.height - y, "eng");
+  console.log(player);
+  player.posY = game.height - y;
+  players.push(player);
+  player.anchor.set(0.5);
+  game.physics.enable(player, Phaser.Physics.ARCADE); 
+  
 }
